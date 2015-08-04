25055af44816163d6752a513edae40991f63efb7
Joshua Brockschmidt <JoshuaBrockschmidt@gmail.com>
2015-08-04 16:36:32 -0700

    Add shader example
    Currently does not display anything

--- src/game.cpp
+++ src/game.cpp
@@ -1,14 +1,18 @@
 #include "game.hpp"
 #include "mesh.hpp"
+#include "resourceloader.hpp"
+#include "shader.hpp"
 #include "vector3.hpp"
 #include "vertex.hpp"
 
 namespace DOIT {
 	namespace Game {
 		Mesh* testMesh;
+		Shader* testShader;
 
 		void init() {
 			testMesh = new Mesh();
+			testShader = new Shader();
 
 			Vertex data[] = {
 				Vertex(Vector3(-1, -1,  0)),
@@ -17,10 +21,14 @@ namespace DOIT {
 			};
 
 			testMesh->addVertices(data);
+			testShader->addVertexShader(ResourceLoader::loadShader("basicVertex.vs"));
+			testShader->addFragmentShader(ResourceLoader::loadShader("basicFragment.fs"));
+			testShader->compileShader();
 		}
 
 		void cleanUp() {
 			delete testMesh;
+			delete testShader;
 		}
 
 		void update() {
@@ -28,6 +36,7 @@ namespace DOIT {
 		}
 
 		void render() {
+			testShader->bind();
 			testMesh->draw();
 		}
 	}
